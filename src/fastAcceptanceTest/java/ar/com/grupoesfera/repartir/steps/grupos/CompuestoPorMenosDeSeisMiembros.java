a53c8b9fdaa849476aabec86c989f1e47d0c421a
Alejandro Balladares <ale_12_river@hotmail.com>
2026-05-18 16:53:24 -0300

    Agrego los steps del escenario 2 de la regla 2

--- src/fastAcceptanceTest/java/ar/com/grupoesfera/repartir/steps/grupos/CompuestoPorMenosDeSeisMiembros.java
+++ src/fastAcceptanceTest/java/ar/com/grupoesfera/repartir/steps/grupos/CompuestoPorMenosDeSeisMiembros.java
@@ -26,11 +26,28 @@ public class CompuestoPorMenosDeSeisMiembros extends FastCucumberSteps {
 
         grupo.setMiembros( miembros );
     }
-    
 
+    @Cuando("el usuario intenta crear un grupo indicando cinco miembros")
+    public void elUsuarioIntentaCrearUnGrupoIndicandoCincoMiembros() {
+
+        List<String> miembros = new LinkedList<String>();
+        miembros.add( "Emilio" );
+        miembros.add( "Emiliano" );
+        miembros.add( "Maximiliano" );
+        miembros.add( "Alex" );
+        miembros.add( "Alexis" );
+        grupo = new Grupo();
+
+        grupo.setMiembros( miembros );
+    }
+    
     @Entonces("no debería crear el grupo con un seis miembros")
     public void noDeberiaCrearElGrupoConUnSeisMiembros() {
-
         assertThat( this.grupo.estaFormado() ).isFalse();
+    }
+
+    @Entonces("visualiza dentro del listado el grupo con los miembros indicados")
+    public void visualizaDentroDelListadoElGrupoConLosMiembrosIndicados() {
+        assertThat( this.grupo.estaFormado() ).isTrue();
     }
 }
