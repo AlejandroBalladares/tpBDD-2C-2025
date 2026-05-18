26320cfa98f8d6e27ee4a154a1af5f979236192b
Alejandro Balladares <ale_12_river@hotmail.com>
2026-05-18 16:19:32 -0300

    Agrego los steps de los escenarios de la regla 2

---
+++ src/fastAcceptanceTest/java/ar/com/grupoesfera/repartir/steps/grupos/CompuestoPorMenosDeSeisMiembros.java
@@ -0,0 +1,35 @@
+package ar.com.grupoesfera.repartir.steps.grupos;
+import ar.com.grupoesfera.repartir.model.Grupo;
+import ar.com.grupoesfera.repartir.steps.FastCucumberSteps;
+import io.cucumber.java.es.Cuando;
+import io.cucumber.java.es.Entonces;
+
+import java.util.*;
+
+import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
+
+public class CompuestoPorMenosDeSeisMiembros extends FastCucumberSteps {
+
+    private Grupo grupo;
+
+    @Cuando("el usuario intenta crear un grupo indicando seis miembros")
+    public void elUsuarioIntentaCrearUnGrupoIndicandoUnUnicoMiembro() {
+
+        List<String> miembros = new LinkedList<String>();
+        miembros.add( "Emilio" );
+        miembros.add( "Emiliano" );
+        miembros.add( "Maximiliano" );
+        miembros.add( "Alex" );
+        miembros.add( "Alexis" );
+        miembros.add( "Alejo" );
+        grupo = new Grupo();
+
+        grupo.setMiembros( miembros );
+    }
+
+    @Entonces("no debería crear el grupo con un seis miembros")
+    public void noDeberiaCrearElGrupoConUnUnicoMiembro() {
+
+        assertThat( this.grupo.estaFormado() ).isFalse();
+    }
+}
